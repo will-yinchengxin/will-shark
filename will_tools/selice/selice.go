08c8073f99948d0fcb78c4ddb6c9ca1f7969ab28
will yin <826895143@qq.com>
2022-10-31 22:42:37 +0800

    feat: add selice-tools

--- will_tools/selice/selice.go
+++ will_tools/selice/selice.go
@@ -1,5 +1,27 @@
 package selice
 
+import "reflect"
+
+func InArray(needle interface{}, haystack interface{}) (exists bool, index int) {
+	exists = false
+	index = -1
+
+	switch reflect.TypeOf(haystack).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(haystack)
+
+		for i := 0; i < s.Len(); i++ {
+			if reflect.DeepEqual(needle, s.Index(i).Interface()) == true {
+				index = i
+				exists = true
+				return
+			}
+		}
+	}
+
+	return
+}
+
 func SliceContains(sl []interface{}, v interface{}) bool {
 	for _, vv := range sl {
 		if vv == v {
