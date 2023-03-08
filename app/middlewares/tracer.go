957b376ed896e2b0cdd1e3ebe72a937fdb91a743
will yin <826895143@qq.com>
2023-03-08 17:24:45 +0800

    feat: add jaeger tools

--- app/middlewares/tracer.go
+++ app/middlewares/tracer.go
@@ -10,7 +10,7 @@ import (
 func StartTrace() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		tr := core.Trace.Tracer(consts.JaegerRouter)
-		ctxRoot, span := tr.Start(context.Background(), c.Request.RequestURI)
+		ctxRoot, span := tr.Start(context.Background(), c.Request.URL.Path)
 		c.Set(consts.JaegerContext, ctxRoot)
 		defer span.End()
 		c.Next()
