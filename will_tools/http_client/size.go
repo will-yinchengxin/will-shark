5676a56bc6b9657630753e43bdbacf9071ba293a
Will Yin <52946059+will-yinchengxin@users.noreply.github.com>
2024-03-06 15:51:31 +0800

    Create size.go

---
+++ will_tools/http_client/size.go
@@ -0,0 +1,27 @@
+package http_client
+
+import "net/http"
+
+func ComputeAppRequestSize(r *http.Request) int {
+	s := 0
+	if r.URL != nil {
+		s = len(r.URL.Path)
+	}
+
+	s += len(r.Method)
+	s += len(r.Proto)
+	for name, values := range r.Header {
+		s += len(name)
+		for _, value := range values {
+			s += len(value)
+		}
+	}
+	s += len(r.Host)
+
+	// N.B. r.Form and r.MultipartForm are assumed to be included in r.URL.
+
+	if r.ContentLength != -1 {
+		s += int(r.ContentLength)
+	}
+	return s
+}
